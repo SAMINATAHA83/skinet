231e5813ffd8776ae79b373d9e03b1aefbbd0b72
YACIN ABDILLAHI <samatar.yacin@hotmail.com>
2022-10-11 23:29:50 -0400

    End of section 3

---
+++ Infrastructure/Data/ProductRepositoy.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data{
+
+    public class ProductReposity : IProductRepository
+    {
+        private readonly StoreContext _context;
+        public ProductReposity(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+           return await _context.ProductBrands.ToListAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
+        }
+    }
+}
